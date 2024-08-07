7b75111a57429ea52b106114329e75cbb0cbe655
Denis Korir <denohrkoryr@gmail.com>
2024-08-07 12:59:15 +0300

    practices

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,39 @@
 import React from "react";
+import MesaVerde from "./parks/MesaVerde";
 import howManyParks from "./parks/howManyParks";
+// import { trees, wildlife } from "./parks/RockyMountain";
+// import * as RMFunctions from "./parks/RockyMountain";
+import {
+  trees as parkTrees,
+  wildlife as parkWildlife,
+} from "./parks/RockyMountain";
+
+console.log(parkTrees);
+
+parkWildlife();
+
+// console.log(RMFunctions.trees);
+
+// RMFunctions.wildlife();
+
+// RMFunctions.elevation();
+
+// console.log(trees);
+
+// wildlife();
+
+// function ColoradoStateParks() {
+//   howManyParks(); // => "42 parks!"
+
+//   return <h1>Colorado State Parks!</h1>;
+// }
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
-
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <div>
+      <MesaVerde />
+    </div>
+  );
 }
+
+export default ColoradoStateParks;
